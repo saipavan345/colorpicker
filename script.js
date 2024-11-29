f59e0b5e848bdaf51754ab65e5f837c07f0f2181
saipavan345 <168091072+saipavan345@users.noreply.github.com>
2024-11-29 17:11:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+function color1()
+{
+    var a=document.getElementById('color');
+   
+    a.style.color="red";
+}
+function color2()
+{
+    var a=document.getElementById('color');
+   
+    a.style.color="blue";
+}
+function color3()
+{
+    var a=document.getElementById('color');
+    a.style.color="orange";
+   
+}
+function color4()
+{
+    var a=document.getElementById('color');
+    a.style.color="pink";
+}
+function color5()
+{
+    var a=document.getElementById('color');
+    a.style.color="green";
+}
+function color6()
+{
+    var a=document.getElementById('color');
+    a.style.color="pink";
+}
+function color7()
+{
+    var a=document.getElementById('color');
+    a.style.color="skyblue";
+}
+function color8()
+{
+    var a=document.getElementById('color');
+    a.style.color="black";
+}
+
+function head1()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily='Brush Script MT';
+}
+function head2()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily='Garamond';
+}
+function head3()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily=' Copperplate, Papyrus, fantasy';
+}
+
+function head4()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily=' Algerian';
+}
+function head5()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily='French Script MT';
+}
+function head6()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily='Wide Latin';
+}
+function head7()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily='Kristen ITC';
+}
+function head8()
+{
+    var a=document.getElementById('color');
+    a.style.fontFamily='Blackadder ITC';
+}
+//lll
+function back1()
+{
+    var a=document.getElementById('color');
+   
+    a.style.backgroundColor="orange";
+}
+
+function back2()
+{
+    var a=document.getElementById('color');
+   
+    a.style.backgroundColor="blue";
+}
+function back3()
+{
+    var a=document.getElementById('color');
+   
+    a.style.backgroundColor="pink";
+}
+function back4()
+{
+    var a=document.getElementById('color');
+   
+    a.style.backgroundColor="green";
+}
+function back5()
+{
+    var a=document.getElementById('color');
+   
+    a.style.backgroundColor="brown";
+}
+
+
+
